37954b6c49b627a2eef71b20e89af082c13e4a0e
iriska <iriska_panina@mail.ru>
2020-05-13 20:33:06 +0300

    week 2 test 1

--- client/components/dashboard_profile.js
+++ client/components/dashboard_profile.js
@@ -5,11 +5,11 @@ const DashboardProfile = () => {
   const { user } = useParams()
   return (
     <div>
-      <div id="title"> PROFILE </div>
+      <div id="title">Profile</div>
       <div id="username"> {user} </div>
-      <Link to="/dashboard/main"> Go To Main </Link>
+      <Link to="/dashboard/main">Go To Main</Link>
       <h2>
-        <Link to="/dashboard"> Go To Root </Link>
+        <Link to="/dashboard">Go To Root</Link>
       </h2>
     </div>
   )
